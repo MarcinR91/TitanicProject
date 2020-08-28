ade2f2a1493165ab75473360a1aef2fa0d1e014f
MarcinR91 <marcinradzyniak@gmail.com>
2020-08-29 00:50:06 +0200

    Second Commit

--- src/titanicproject/TitanicProject.java
+++ src/titanicproject/TitanicProject.java
@@ -16,23 +16,48 @@ public class TitanicProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    
+    //essential variables    
     String sex;
     int age;
+    int chances = 0;
     String ifparent;
     String onlyparent;
         
 // TODO code application logic here
         Scanner input = new Scanner(System.in);
-        // zbieranie informacji
+        // gathering information
         System.out.println("What is your gender?");
         sex = input.nextLine();
-        System.out.println("What is your age?");
-        age = input.nextInt();
         System.out.println("Are you a parent?");
         ifparent = input.nextLine();
         System.out.println("Are you single parent?");
         onlyparent = input.nextLine();
+        System.out.println("What is your age?");
+        age = input.nextInt();
         
+        
+        //logic
+        if(sex .equals("female")){
+            chances = 5;
+        }else{
+        chances = 1;
+        }
+        if(age < 15){
+            chances = (chances + 10);
+        }
+        if(ifparent .equals("yes")){
+            chances++;
+        }
+        if(onlyparent .equals("yes")){
+            chances++;
+        }
+        
+        if(chances < 5){
+        System.out.println("You're chances are very low");
+    }else{
+       System.out.println("You're safe");
+        }
     }
     
 }
