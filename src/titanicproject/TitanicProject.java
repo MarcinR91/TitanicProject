c6d75ddcd6470884faa4a44b4d20143311b572a6
MarcinR91 <marcinradzyniak@gmail.com>
2020-08-29 00:09:49 +0200

    First.1 Commit

--- src/titanicproject/TitanicProject.java
+++ src/titanicproject/TitanicProject.java
@@ -4,22 +4,34 @@
  * and open the template in the editor.
  */
 package titanicproject;
-
+import java.util.Scanner;
 /**
  *
  * @author marcinr
  */
 public class TitanicProject {
 
-    int plec;
-    int wiek;
-    int rodzic;
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    String sex;
+    int age;
+    String ifparent;
+    String onlyparent;
+        
+// TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        // zbieranie informacji
+        System.out.println("What is your gender?");
+        sex = input.nextLine();
+        System.out.println("What is your age?");
+        age = input.nextInt();
+        System.out.println("Are you a parent?");
+        ifparent = input.nextLine();
+        System.out.println("Are you single parent?");
+        onlyparent = input.nextLine();
         
     }
     
